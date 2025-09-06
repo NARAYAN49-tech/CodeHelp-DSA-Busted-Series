e826679cb6a9a603d9797758f3b1e184c8732b76
Narayan Rajkumar Suryawanshi <178514243+NARAYAN49-tech@users.noreply.github.com>
2025-09-06 20:43:28 +0530

    Update firstProgram.cpp

--- Lecture002 Write First C++ Program/firstProgram.cpp
+++ Lecture002 Write First C++ Program/firstProgram.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
     
   cout << " Namaste Dunia " << endl;
-  
+  cout << "Hello World" << endl;
+  cout << "Addition of 2 + 4  = " << 2 + 4 << endl;
   return 0; 
 }
